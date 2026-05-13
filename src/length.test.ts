a94a207d379c17347ae072cd68aa862dc19e1be5
symphony <symphony@local>
2026-05-13 07:13:23 -0300

    chore(e2e): scaffold vague-issue

--- src/length.test.ts
+++
@@ -1,7 +0,0 @@
-import { test } from "node:test";
-import assert from "node:assert/strict";
-import { safeLength } from "./length.ts";
-
-test("safeLength: string", () => {
-  assert.equal(safeLength("hi"), 2);
-});
