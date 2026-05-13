5392f3c73f3fe3dbfc7836f0a822029a842b5b77
symphony <symphony@local>
2026-05-13 06:40:22 -0300

    chore(e2e): scaffold off-by-one

---
+++ src/find.test.ts
@@ -0,0 +1,7 @@
+import { test } from "node:test";
+import assert from "node:assert/strict";
+import { firstIndexOf } from "./find.ts";
+
+test("firstIndexOf: absent target returns -1", () => {
+  assert.equal(firstIndexOf([1, 2, 3], 99), -1);
+});
